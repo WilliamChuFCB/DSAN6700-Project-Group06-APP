6ff0848319e073d83c383c1f8fb98696902edfd0
kefanyu <59634672+kefanyu@users.noreply.github.com>
2023-11-01 15:45:40 -0400

    app deployment fin

---
+++ app.py
@@ -0,0 +1,30 @@
+#%%
+import numpy as np
+from flask import Flask, request,render_template
+import pickle
+
+#%%
+app = Flask(__name__)
+model = pickle.load(open('samplehptuning.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+ 
+    string_lst = eval(request.form.get("variable"))
+    input_feature = [float(i) for i in string_lst]
+    input_feature = [np.array(input_feature)]
+    pred = model.predict(input_feature)
+
+    if pred[0] == 0:
+        pred = "False"
+    elif pred[0] == 1:
+        pred = "True"
+
+    return render_template('index.html', prediction_text='People with these features have heart attack: {}'.format(pred))
+
+if __name__ == "__main__":
+    app.run(debug=True)
